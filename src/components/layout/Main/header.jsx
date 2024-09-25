fc804d63ab7c338b455990a9caa38a060acc8637
Quynh Nga <128832470+qnga5523@users.noreply.github.com>
2024-09-25 11:26:24 +0700

    update
    update

--- src/components/layout/Main/header.jsx
+++ src/components/layout/Main/header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Layout, Menu } from "antd";
 import { Link } from "react-router-dom";
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar, Space } from "antd";
 
 const { Header } = Layout;
 
@@ -38,7 +40,18 @@ export default function HeaderHomepage() {
           <Menu.Item key="contact">
             <Link to="/contact">Contact</Link>
           </Menu.Item>
+          <Menu.Item key="signup">
+            <Link to="/signup">Signup</Link>
+          </Menu.Item>
+          <Menu.Item key="login">
+            <Link to="/login">Login</Link>
+          </Menu.Item>
         </Menu>
+        <Space direction="vertical" size={16}>
+          <Space wrap size={16}>
+            <Avatar size={64} icon={<UserOutlined />} />
+          </Space>
+        </Space>
       </Header>
     </Layout>
   );
